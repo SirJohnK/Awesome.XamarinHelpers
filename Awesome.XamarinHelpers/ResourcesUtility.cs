8dd79f2ea01c17b3719bc0a07541eaf2abce975d
SirJohnK <SirJohnK@users.noreply.github.com>
2019-11-27 00:46:01 +0100

    Add project files.

---
+++ Awesome.XamarinHelpers/ResourcesUtility.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+namespace Awesome.XamarinHelpers
+{
+    public static class ResourcesUtility
+    {
+        private static T GetResource<T>(object? resource)
+        {
+            //Verify Parameter
+            if (resource == null)
+                throw new ArgumentNullException(nameof(resource));
+
+            //Check resource type
+            switch (resource)
+            {
+                case OnPlatform<T> value:
+                    //Get platform specific resource
+                    return GetResource<T>(value.Platforms?.FirstOrDefault(p => p.Platform.Contains(Device.RuntimePlatform))?.Value);
+
+                case DynamicResource value:
+                    //Get dynamic resource
+                    return FindResource<T>(value.Key);
+
+                case string value:
+                    {
+                        //Attempt to cast to resource type or return default
+                        try
+                        {
+                            return (T)Convert.ChangeType(value, typeof(T));
+                        }
+                        catch
+                        {
+                            return default;
+                        }
+                    }
+
+                case T value:
+                    //Return found resource value
+                    return value;
+
+                default:
+                    //Return default resource value
+                    return default;
+            }
+        }
+
+        public static T FindResource<T>(string resourceKey)
+        {
+            //Verify parameter
+            if (string.IsNullOrWhiteSpace(resourceKey))
+                return default;
+
+            //Attempt to find resource
+            if (Application.Current.Resources.TryGetValue(resourceKey, out var resource))
+                //Get resource
+                return GetResource<T>(resource);
+            else
+                //If nothing found, return default
+                return default;
+        }
+    }
+}
---
+++ Tests/ResourcesTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class ResourcesTests
+    {
+        [TestMethod]
+        public void FindResource_ShouldReturnCorrectTypeAndValue_WithSimpleDynamicResource()
+        {
+        }
+    }
+}
